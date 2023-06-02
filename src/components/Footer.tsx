b7f02c683e8100b3f413a1f58d70dfd690b4f4bb
funbiioye <funbiioye@gmail.com>
2023-06-02 07:21:02 +0100

    page footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,25 @@
 import pishon from '../assets/Icons/Pishon (4).svg';
+import twitter from '../assets/Icons/twitter.svg'
+import instagram from '../assets/Icons/instagram.svg';
+import facebook from '../assets/Icons/facebook.svg';
+import youtube from '../assets/Icons/youtube.svg';
+import linkedin from '../assets/Icons/linkedin.svg';
 
 const Footer = () => {
   return (
-    <div className='w-full h-[938px] bg-[#00359E] px-[16px] pt-[48px]'>
-      <div className='flex flex-col md:flex md:flex-row md:gap-12'>
+    <div className='w-full h-[938px] bg-[#00359E] px-[16px] pt-[48px] md:px-[40px] font-inter text-[#ffffff]'>
+      <div className='flex flex-col md:flex md:flex-row md:gap-12 border-b-[1px] border-[#ffffff] '>
         <div className='bg-[#00359E] md:flex-[0.40]'>
           <img
             src={pishon}
             alt='Pishon logo'
           />
-          <p className=' h-[48px] mt-[24px] md:mt-[32px] '>
-            Buy shares of rental properties in under 5 <br></br> minutes starting from
+          <p className=' h-[48px] mt-[24px] md:mt-[32px] font-normal text-base '>
+            Buy shares of rental properties in under 5 minutes starting from
             $10.
           </p>
         </div>
-        <div className='grid grid-cols-2 md:grid-cols-5 gap-8 md:flex-[0.60] my-[48px]'>
+        <div className='grid grid-cols-2 md:grid-cols-5 gap-8 md:flex-[0.60] my-[48px] font-semibold text-base'>
           <span className='w-[140.8px] '>
             <ul>
               <li className='pb-[16px]'>Product</li>
@@ -59,27 +64,32 @@ const Footer = () => {
           </span>
         </div>
       </div>
-      <div className=''>
-        <p>© 2023 Pishon Inc. All rights reserved.</p>
-        <span className=''>
+      <div className='flex flex-col-reverse md:flex-row justify-between'>
+        <p className=' mt-[24px] md:mt-[48px] font-normal text-base'>© 2023 Pishon Inc. All rights reserved.</p>
+        <span className='flex flex-row mt-[24px] md:mt-[48px]'>
           <img
-            src=''
+          className='w-[24px] h-[24px] md:ml-[24px]'
+            src={twitter}
             alt=''
           />
           <img
-            src=''
+          className='w-[24px] h-[24px] ml-[24px]'
+            src={linkedin}
             alt=''
           />
           <img
-            src=''
+          className='w-[24px] h-[24px] ml-[24px]'
+            src={facebook}
             alt=''
           />
           <img
-            src=''
+          className='w-[24px] h-[24px] ml-[24px]'
+            src={youtube}
             alt=''
           />
           <img
-            src=''
+          className='w-[24px] h-[24px] ml-[24px]'
+            src={instagram}
             alt=''
           />
         </span>
